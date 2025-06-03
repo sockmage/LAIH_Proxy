63553cf02103a146e425314bffbad3ed6fe484de
cockmage <cockmage@icloud.com>
2025-06-03 14:53:37 +0300

    Revert "Remove trailing whitespace in server.js"
    This reverts commit a37d0583e053181d282d848a7c572998f4643094.

--- server.js
+++ server.js
@@ -280,4 +280,5 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
   process.exit(1);
 });
+
 console.log('Lingro Proxy server starting...');
